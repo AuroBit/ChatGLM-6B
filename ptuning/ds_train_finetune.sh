2eccf9bf49be7cbb045c4038120c74e61fa6f8c7
zhohn0 <540583339@qq.com>
2023-06-30 14:38:36 +0800

    Update finetune params.

--- ptuning/ds_train_finetune.sh
+++ ptuning/ds_train_finetune.sh
@@ -16,7 +16,7 @@ deepspeed --num_gpus=1 --master_port $MASTER_PORT main.py \
     --overwrite_output_dir \
     --max_source_length 64 \
     --max_target_length 64 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --predict_with_generate \
@@ -24,5 +24,5 @@ deepspeed --num_gpus=1 --master_port $MASTER_PORT main.py \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate $LR \
-    --quantization_bit 8
+    --quantization_bit 4
 
